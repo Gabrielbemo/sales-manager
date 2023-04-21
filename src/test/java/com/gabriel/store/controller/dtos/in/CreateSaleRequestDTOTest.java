7d24a6f5c0ac9609099cb4b2f755c5d6df092a7e
Gabriel Bernardes de Moura <gabrielbemoura@gmail.com>
2023-04-21 18:32:15 -0300

    Adicionado testes para DTO's de vendas

---
+++ src/test/java/com/gabriel/store/controller/dtos/in/CreateSaleRequestDTOTest.java
@@ -0,0 +1,65 @@
+package com.gabriel.store.controller.dtos.in;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.gabriel.store.controllers.dtos.in.CreateSaleRequestDTO;
+import com.gabriel.store.models.Sale;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateSaleRequestDTOTest {
+    private Validator validator;
+
+    CreateSaleRequestDTO createSaleRequestDTO;
+
+    @BeforeEach
+    public void setUp() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+
+        createSaleRequestDTO = CreateSaleRequestDTO.builder()
+                .saleDate(LocalDate.now())
+                .saleValue(123L)
+                .sellerId(1L)
+                .build();
+    }
+
+    @Test
+    public void convertToEntity_WithValidData_ReturnsSale() {
+        Sale sale = createSaleRequestDTO.toEntity();
+
+        Assertions.assertEquals(sale.getSaleDate(), createSaleRequestDTO.getSaleDate());
+        Assertions.assertEquals(sale.getSaleValue(), createSaleRequestDTO.getSaleValue());
+        Assertions.assertEquals(sale.getSeller().getId(), createSaleRequestDTO.getSellerId());
+    }
+
+    @Test
+    public void validateDTO_WithValidData_ValidationsReturnsEmpty() {
+        Set<ConstraintViolation<CreateSaleRequestDTO>> violations = validator.validate(createSaleRequestDTO);
+
+        Assertions.assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void validateDTO_WithInvalidData_ValidationsReturnsNotEmpty() {
+        createSaleRequestDTO.setSaleDate(null);
+        createSaleRequestDTO.setSaleValue(null);
+        createSaleRequestDTO.setSellerId(null);
+
+        Set<ConstraintViolation<CreateSaleRequestDTO>> violations = validator.validate(createSaleRequestDTO);
+
+        Assertions.assertFalse(violations.isEmpty());
+        Assertions.assertEquals(violations.size(), 3);
+    }
+}
---
+++ src/test/java/com/gabriel/store/controller/dtos/out/CreateSaleResponseDTOTest.java
@@ -0,0 +1,63 @@
+package com.gabriel.store.controller.dtos.out;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.gabriel.store.controllers.dtos.out.CreateSaleResponseDTO;
+import com.gabriel.store.models.Sale;
+
+public class CreateSaleResponseDTOTest {
+    private Validator validator;
+
+    CreateSaleResponseDTO createSaleResponseDTO;
+
+    @BeforeEach
+    public void setUp() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+
+        createSaleResponseDTO = CreateSaleResponseDTO.builder()
+                .id(1L)
+                .saleDate(LocalDate.now())
+                .build();
+    }
+
+    @Test
+    public void convertFromEntity_WithValidData_ReturnsCreateSaleResponseDTO() {
+        Sale sale = Sale.builder()
+                .id(1L)
+                .saleDate(LocalDate.now())
+                .build();
+        CreateSaleResponseDTO testedSaleResponseDTO = CreateSaleResponseDTO.fromEntity(sale);
+
+        Assertions.assertEquals(sale.getSaleDate(), testedSaleResponseDTO.getSaleDate());
+        Assertions.assertEquals(sale.getId(), testedSaleResponseDTO.getId());
+    }
+
+    @Test
+    public void validateDTO_WithValidData_ValidationsReturnsEmpty() {
+        Set<ConstraintViolation<CreateSaleResponseDTO>> violations = validator.validate(createSaleResponseDTO);
+
+        Assertions.assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void validateDTO_WithInvalidData_ValidationsReturnsNotEmpty() {
+        createSaleResponseDTO.setSaleDate(null);
+        createSaleResponseDTO.setId(null);
+
+        Set<ConstraintViolation<CreateSaleResponseDTO>> violations = validator.validate(createSaleResponseDTO);
+
+        Assertions.assertFalse(violations.isEmpty());
+        Assertions.assertEquals(violations.size(), 2);
+    }
+}
