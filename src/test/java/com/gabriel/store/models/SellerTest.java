5711ee4de8648135d2cfcfc6599187cc0e6c6895
Gabriel Bernardes de Moura <gabrielbemoura@gmail.com>
2023-04-23 20:06:02 -0300

    Adicionado testes para model de vendedores

---
+++ src/test/java/com/gabriel/store/models/SellerTest.java
@@ -0,0 +1,48 @@
+package com.gabriel.store.models;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+public class SellerTest {
+
+    private Validator validator;
+
+    private Seller seller;
+
+    @BeforeEach
+    public void setUp() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+
+        seller = Seller.builder()
+                .id(1L)
+                .name("test1")
+                .salesAmount(1L)
+                .build();
+    }
+
+    @Test
+    public void validateSeller_WithValidData_ViolationsReturnsEmpty() {
+        Set<ConstraintViolation<Seller>> violations = validator.validate(seller);
+
+        Assertions.assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void validateSeller_WithInvalidData_ViolationsReturnsNotEmpty() {
+        seller.setName(null);
+
+        Set<ConstraintViolation<Seller>> violations = validator.validate(seller);
+
+        Assertions.assertFalse(violations.isEmpty());
+        Assertions.assertEquals(violations.size(), 1);
+    }
+}
